e66df827bea00f67d11c89317bd2518e34af35cd
ychng <ychng@student.42kl.edu.my>
2024-04-21 04:54:44 +0800

    Feature: Add new header files for seperating system and library headers

---
+++ includes/sys_headers.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sys_headers.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ychng <ychng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/21 04:43:44 by ychng             #+#    #+#             */
+/*   Updated: 2024/04/21 04:47:21 by ychng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SYS_HEADERS
+# define SYS_HEADERS
+
+// Standard C library headers
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdint.h>
+# include <stdbool.h>
+
+// System-specific headers
+# include <unistd.h>
+
+#endif
